4e7c3e61f3569f212dd89e7809b7388e1c5f4a86
pierrchen <pierr.chen@gmail.com>
2011-01-06 16:11:32 +0800

    be able to search album

--- lib/doubapi.rb
+++ lib/doubapi.rb
@@ -9,9 +9,9 @@ module Doubapi
 #use Douban API
 def self.douban_get_xml url
 	puts url
-    Nokogiri::HTML(open(url,'User-Agent' => 'ruby'),nil, "utf-8")
+    doc = open(url,'User-Agent' => 'ruby')
+    Nokogiri::HTML(doc,nil, "utf-8")
     #Nokogiri::HTML(open(url,:proxy => nil,'User-Agent' => 'ruby'),nil, "utf-8")
-	
 	#no network access, used to simulator 
 	#doc = File.read(File.join(RAILS_ROOT, "app","controllers","event_sample.xml"))
 	#Nokogiri::HTML(doc,nil, "utf-8")
@@ -22,7 +22,7 @@ end
 
 #return Atom
 #Douban search : will return results that does not match 
-def self.search key_chinese, location = "shanghai"
+def self.search_event key_chinese, location = "shanghai"
   keywords= "%" + key_chinese.each_byte.map {|c| c.to_s(16)}.join("%")
   uri="http://api.douban.com/events?q=#{keywords}&location=#{location}&start-index=1&max-results=5"
   #Let's grab it slowly to avoid being baned...	
@@ -30,8 +30,16 @@ def self.search key_chinese, location = "shanghai"
   douban_get_xml(uri)
 end
 
+def self.search_ablum artist_chinese
+  keywords= "%" + artist_chinese.each_byte.map {|c| c.to_s(16)}.join("%")
+  uri="http://api.douban.com/music/subjects?tag=#{keywords}&start-index=1&max-results=5"
+  #Let's grab it slowly to avoid being baned...	
+  sleep(7) 	
+  douban_get_xml(uri)
+end
 
-Douban_Event =  Struct.new :title, :when, :where, :what,:link
+Douban_Event =  Struct.new :title, :when, :where, :what, :link
+Douban_Album =  Struct.new :author, :title, :release_date,  :link
 
 
 #TODO
@@ -46,8 +54,23 @@ def self.looks_like_a_live_show? e, artist
 	return false
 end
 
+
+def self.search_album_of artist
+	doc = search_ablum artist
+	albums=[]
+	doc.xpath("//entry").each do |entry|
+		title = entry.at_xpath(".//title").text
+		author = entry.at_xpath(".//name").text
+		release_date = entry.at_xpath(".//attribute[@name='pubdate']").text
+    	link =  entry.at_xpath(".//link[@rel='alternate']")["href"]
+		puts "#{author} #{title} #{release_date}"
+    	albums << Douban_Album.new(author, title, release_date, link)
+	end
+	albums
+end
+
 def self.search_events_of artist
-  doc = search artist
+  doc = search_event artist
   events=[]
   doc.xpath("//entry").each do |entry|
     #pp entry
--- spec/doubapi_spec.rb
+++ spec/doubapi_spec.rb
@@ -9,6 +9,16 @@ describe Doubapi do
 		end	
 	end
 
+	it "should be able to search albums of 李志 " do 
+		author = "李志"
+		Doubapi.search_album_of(author).each do |album|
+			puts album.author	
+			puts album.title	
+			puts album.release_date	
+			puts album.link	
+		end
+	end
+
 
 
 end
