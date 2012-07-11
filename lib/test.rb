aecb7a0931e124a5040466cfde457779af58c3d7
Pierr Chen <pierr.chen@gmail.com>
2012-07-11 10:50:56 +0800

    add test.rb

---
+++ lib/test.rb
@@ -0,0 +1,62 @@
+#encoding: utf-8
+
+#use local lib instead of the Gem installed
+require './doubapi'
+
+
+def test1
+  key = "许巍" 
+  Doubapi.search_events_of(:key=>key).each do |event|
+    event.what.should be_include(key)
+    puts event.title
+    puts event.when
+    puts event.where
+    puts event.link
+  end	
+
+end
+
+
+def test2
+  author = "李志"
+  Doubapi.search_albums_of(:singer=>author,:since=>"2010-05").each do |album|
+    puts album.author	
+    puts album.release_date	
+    puts album.title	
+    puts album.link	
+  end
+end 
+
+
+
+def test3
+  
+  puts "1-30"
+  Doubapi.search_events_of(:key => "all", :location => "shanghai", :start_index => 1,:max_result => 30).each do |event|
+    puts "#{event.when} #{event.title}"
+    #puts event.where
+    #puts event.link
+  end
+  
+  puts "1-15"
+  
+  Doubapi.search_events_of(:key => "all", :location => "shanghai", :start_index => 1,:max_result => 15).each do |event|
+    puts "#{event.when} #{event.title}"
+    #puts event.where
+    #puts event.link
+  end
+  
+  puts "16-30"
+  
+  Doubapi.search_events_of(:key => "all", :location => "shanghai", :start_index => 16,:max_result => 15).each do |event|
+    puts "#{event.when} #{event.title}"
+    #puts event.where
+    #puts event.link
+  end
+end
+
+
+#test3
+#test1
+test2
+
